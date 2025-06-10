47841218ac9f64110efa5caa651b8502ebf2a9c9
Mateus Paulart <mateusp23@rede.ulbra.br>
2025-06-10 09:53:54 -0300

    ajustes layout

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,48 +1,25 @@
-import { Inter, Roboto } from 'next/font/google';
-import { ReactNode } from 'react';
+// app/[locale]/layout.tsx
+
 import { NextIntlClientProvider } from 'next-intl';
 import { notFound } from 'next/navigation';
-
-const inter = Inter({
-  variable: '--font-inter',
-  subsets: ['latin'],
-  display: 'swap',
-});
-
-const roboto = Roboto({
-  variable: '--font-roboto',
-  subsets: ['latin'],
-  weight: ['400', '700'],
-  display: 'swap',
-});
-
-export function generateStaticParams() {
-  return [{ locale: 'pt' }, { locale: 'en' }, { locale: 'es' }];
-}
 
 export default async function LocaleLayout({
   children,
   params,
 }: {
-  children: ReactNode;
+  children: React.ReactNode;
   params: { locale: string };
 }) {
-  const { locale } = params;
-
   let messages;
   try {
-    messages = (await import(`../../messages/${locale}/common.json`)).default;
+    messages = (await import(`../../messages/${params.locale}/common.json`)).default;
   } catch (error) {
-    notFound(); // para lidar com locale inválido
+    notFound();
   }
 
   return (
-    <html lang={locale} className={`${inter.variable} ${roboto.variable}`}>
-      <body className="bg-background antialiased">
-        <NextIntlClientProvider locale={locale} messages={messages}>
-          {children}
-        </NextIntlClientProvider>
-      </body>
-    </html>
+    <NextIntlClientProvider locale={params.locale} messages={messages}>
+      {children}
+    </NextIntlClientProvider>
   );
 }
--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,3 +1,5 @@
+'use-client'
+
 import { useTranslations } from 'next-intl';
 import AboutUs from "../components/AboutUs";
 import Contact from "../components/Contact";
