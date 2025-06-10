77080186f3d3d823145105ba24bf8f3c881b2c59
Mateus Paulart <mateusp23@rede.ulbra.br>
2025-06-10 09:22:42 -0300

    ajustes gerais idioma e layouts

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -2,24 +2,40 @@
 
 import { NextIntlClientProvider } from 'next-intl';
 import { notFound } from 'next/navigation';
+import { ReactNode } from 'react';
+import { Inter, Roboto } from 'next/font/google';
 
-export default async function LocaleLayout({
-  children,
-  params: { locale }
-}: {
-  children: React.ReactNode;
+const inter = Inter({
+  variable: '--font-inter',
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+const roboto = Roboto({
+  variable: '--font-roboto',
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  display: 'swap',
+});
+
+type Props = {
+  children: ReactNode;
   params: { locale: string };
-}) {
-  let messages;
+};
+
+export default async function LocaleLayout({ children, params }: Props) {
+  const { locale } = params;
+
+  let messages: Record<string, string>;
   try {
     messages = (await import(`../../messages/${locale}/common.json`)).default;
   } catch (error) {
-    notFound();
+    return notFound(); // importante usar "return" aqui
   }
 
   return (
-    <html lang={locale}>
-      <body>
+    <html lang={locale} className={`${inter.variable} ${roboto.variable}`}>
+      <body className="bg-background antialiased">
         <NextIntlClientProvider locale={locale} messages={messages}>
           {children}
         </NextIntlClientProvider>
